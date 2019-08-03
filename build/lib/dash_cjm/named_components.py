c82f4e0184f6213106a81dedf90db7aae5a267e5
cjmcclellan <cmcclellan1993@gmail.com>
2019-08-03 00:20:07 -0700

    Adding styling to the named components 2

--- build/lib/dash_cjm/named_components.py
+++ build/lib/dash_cjm/named_components.py
@@ -2,30 +2,45 @@ import dash_core_components as dcc
 import dash_html_components as html
 
 
-def NamedInput(name, **kwargs):
+def NamedInput(name, style=None, **kwargs):
+    style = __get_style(style)
     return html.Div([
-        html.P('{}:'.format(name), style={'margin-left': '3px'}),
-        dcc.Input(**kwargs)
+        html.P('{}:'.format(name), style=style),
+        dcc.Input(**kwargs, style=style)
         ])
 
 
-def NamedDropdown(name, **kwargs):
+def NamedDropdown(name, style=None, **kwargs):
+    style = __get_style(style)
     return html.Div([
-        html.P('{}:'.format(name), style={'margin-left': '3px'}),
+        html.P('{}:'.format(name), style=style),
         dcc.Dropdown(**kwargs)
         ])
 
 
-def NamedSlider(name, **kwargs):
+def NamedSlider(name, style=None, **kwargs):
+    style = __get_style(style)
     return html.Div([
-        html.P('{}:'.format(name), style={'margin-left': '3px'}),
+        html.P('{}:'.format(name), style=style),
         dcc.Slider(**kwargs)
         ])
 
 
-def NamedRadio(name, **kwargs):
+def NamedRadio(name, style=None, **kwargs):
+    style = __get_style(style)
     return html.Div([
-        html.P('{}:'.format(name), style={'margin-left': '3px'}),
+        html.P('{}:'.format(name), style=style),
         dcc.RadioItems(**kwargs)
         ])
 
+
+def __get_style(style):
+    # default_style = {'margin-left': '3px'}
+    default_style = {}
+    if style is None:
+        return default_style
+    else:
+        for key in default_style.keys():
+            if style.get(key, None) is None:
+                style[key] = default_style[key]
+        return style
